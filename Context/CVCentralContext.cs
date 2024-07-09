b8c22731fc2bbdb2bec78b2b09371399a179e0c6
Juan Pablo <juanpablolc414@gmail.com>
2024-07-09 03:09:55 -0500

    Initial Commit

---
+++ Context/CVCentralContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+namespace CV_Central.Context
+{
+    public class CVCentralContext : DbContext
+    {
+        public CVCentralContext(DbContextOptions<CVCentralContext> options) : base(options){
+            
+        }
+    }
+}
