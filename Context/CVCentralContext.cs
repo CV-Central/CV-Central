41074753e25acb6796886ce1fbe61343196dd5b1
IsaGuzman55 <isabellaguzman143@gmail.com>
2024-07-22 20:43:05 -0500

    Crear llaves primarias compuestas de las tablas que no tienen Id propio RoleHasPermissions, UserHasPermissions y UserHasRoles

--- Context/CVCentralContext.cs
+++ Context/CVCentralContext.cs
@@ -17,5 +17,29 @@ namespace CV_Central.Context
         public DbSet<UserHasRole> UserHasRoles {get; set;}
         public DbSet<WorkExperience> WorkExperiences {get; set;}
 
+        /* CLAVE PRIMARIA COMPUESTA */
+        /* El metodo "OnModelCreating" es propio del "DbContext" */
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            /* RoleHasPermissions */
+            /* Crear una clave primaria compuesta para la entidad "RoleHasPermissions" a partir de PermissionId y RoleId*/
+            modelBuilder.Entity<RoleHasPermission>()
+                .HasKey(rp => new { rp.PermissionId, rp.RoleId });
+            
+            /* UserHasPermissions */
+            /* Crear una clave primaria compuesta para la entidad "UserHasPermissions" a partir de PermissionId, UserType y UserId*/
+            modelBuilder.Entity<UserHasPermission>()
+                .HasKey(up => new { up.PermissionId, up.UserType, up.UserId});
+            
+            /* UserHasRoles */
+            /* Crear una clave primaria compuesta para la entidad "UserHasPermissions" a partir de RoleId, UserType y UserId*/
+            modelBuilder.Entity<UserHasRole>()
+                /*  La combinación de estas dos propiedades(StudentId, CourseId) será la clave primaria de la entidad "StudentCourse". */
+                .HasKey(ur => new { ur.RoleId, ur.UserType, ur.UserId});
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+       
     }
 }
